2eb2d1a3896131d4c69ad45ea456fcd05dbcc07f
tinyuliu <tinyu1016@gmail.com>
2021-07-08 00:32:19 +0800

    Making custom context provider v2

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -36,6 +37,8 @@ const Login = (props) => {
     value: "",
     isValid: false
   });
+
+  const authCtx = useContext(AuthContext);
 
   // imporve the useEffect dependencies
   const { isValid: emailIsValid } = emailState;
@@ -74,7 +77,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
